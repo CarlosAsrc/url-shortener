ae255576c4a03e87c4dd5717fbd9e02d8b4a0bb4
carlosasrc <carlos.asrc@gmail.com>
2024-08-18 17:43:20 -0300

    infra: add dynamodb table creation in terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,6 +111,64 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   role = aws_iam_role.ec2_instance_role.name
 }
 
+
+// DynamoDB:
+resource "aws_dynamodb_table" "url_table" {
+  name           = "url-development"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "shortUrl"
+
+  global_secondary_index {
+    name               = "LongUrlIndex"
+    hash_key           = "longUrl"
+    projection_type    = "ALL"
+
+    read_capacity  = 10
+    write_capacity = 10
+  }
+
+    global_secondary_index {
+    name               = "CreatedAtIndex"
+    hash_key           = "createdAt"
+    projection_type    = "ALL"
+
+    read_capacity  = 5
+    write_capacity = 5
+  }
+
+  global_secondary_index {
+    name               = "AccessCountIndex"
+    hash_key           = "accessCount"
+    projection_type    = "ALL"
+
+    read_capacity  = 5
+    write_capacity = 5
+  }
+
+  attribute {
+    name = "shortUrl"
+    type = "S"
+  }
+
+  attribute {
+    name = "longUrl"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  attribute {
+    name = "accessCount"
+    type = "N"
+  }
+}
+
+
 // EC2 Instance:
 resource "aws_instance" "golang_app" {
   ami             = "ami-0ba9883b710b05ac6"
