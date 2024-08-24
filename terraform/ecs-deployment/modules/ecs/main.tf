85fd4dbefabdd2556637027f0c71e09df272dafc
carlosasrc <carlos.asrc@gmail.com>
2024-08-24 18:41:49 -0300

    feat: add health check configs

--- terraform/ecs-deployment/modules/ecs/main.tf
+++ terraform/ecs-deployment/modules/ecs/main.tf
@@ -63,7 +63,7 @@ resource "aws_ecs_task_definition" "task" {
           "awslogs-stream-prefix" = "ecs"
         }
       }
-      health_check {
+      health_check = {
         command     = ["CMD-SHELL", "curl -f http://localhost:8080/health || exit 1"]
         interval    = 30
         timeout     = 10
@@ -109,7 +109,9 @@ resource "aws_lb_target_group" "ecs_tg" {
 
 
   health_check {
-    path = "/"
+    protocol = "HTTP"
+    port     = 8080
+    path     = "/"
   }
 }
 
